6c95789029c384aa7102f2d818d66b238973043a
unknown <pratikshakandel100@gmail.com>
2025-05-22 21:57:48 +0545

    :sparkles: file added

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+function Title(){
+  return <h1>ROJGARIHUB</h1>
+}
+function App() {
+  return <div>
+    <p>I am here.</p>
+    <Title></Title>
+    <h1>Hello</h1>
+    </div>;
+}
+export default App ;
+
